d69de58e98c81dddfcb94334318c20bb8211f61f
Dev <dev@Patricks-MacBook-Pro.local>
2018-03-10 10:33:04 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavItem, Nav } from "react-bootstrap";
+import Routes from "./Routes";
+import RouteNavItem from "./components/RouteNavItem";
+import { authUser, signOutUser } from "./libs/awsLib";
+import "./App.css";
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      if (await authUser()) {
+        this.userHasAuthenticated(true);
+      }
+    }
+    catch(e) {
+      alert(e);
+    }
+  
+    this.setState({ isAuthenticating: false });
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    signOutUser();
+  
+    this.userHasAuthenticated(false);
+  }
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+  
+    return (
+      !this.state.isAuthenticating &&
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : [
+                    <RouteNavItem key={1} href="/signup">
+                      Signup
+                    </RouteNavItem>,
+                    <RouteNavItem key={2} href="/login">
+                      Login
+                    </RouteNavItem>
+                  ]}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    cognito: {
+      USER_POOL_ID: "us-east-2_MjjDzK0Q8",
+      APP_CLIENT_ID: "1m7f0vt66d3nvlkbpbjdo7suvt"
+    }
+  };
